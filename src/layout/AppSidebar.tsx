87f47f92c752e23fbe98a1f24940559327e2c3f6
jobair-talha <talhajobair10@gmail.com>
2025-09-05 11:11:45 +0600

    Add API service for slider management: implement CRUD operations and query handling

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -351,14 +351,14 @@ const AppSidebar: React.FC = () => {
             <>
               <img
                 className="dark:hidden"
-                src="/images/logo/logo.svg"
+                src="/images/logo/logo.png"
                 alt="Logo"
                 width={150}
                 height={40}
               />
               <img
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                src="/images/logo/logo.png"
                 alt="Logo"
                 width={150}
                 height={40}
